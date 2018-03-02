f9445f0b6a563de543ad323339bf52b5511d3995
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-02 12:28:02 -0500

    import promise, react-router-dom components, apply promise as middleware, define root application route and component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Route exact path="/" component={PostsIndex} />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
